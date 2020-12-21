cc01c0585c1f2671944b19b51153a82a4f144856
dsrees <daniel.rees18@gmail.com>
2020-12-21 11:51:08 -0500

    Fixed git auto completion

--- oh-my-zsh/oh-my-zsh.zsh
+++ oh-my-zsh/oh-my-zsh.zsh
@@ -1,14 +1,14 @@
-# Path to your oh-my-zsh configuration.
-ZSH=$HOME/.oh-my-zsh
-
-# Use crunch theme
+# Using custom prompt setup located in /zsh/prompt.zsh but
+# if you ever want to change your theme, set it here
 # ZSH_THEME="materialshell"
 
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
+# # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# # Example format: plugins=(rails git textmate ruby lighthouse)
 plugins=(git asdf zsh-syntax-highlighting kubectl)
 
+# Source the oh my zsh shell. $ZSH is defined in oh-my-zsh/path.zsh
 source $ZSH/oh-my-zsh.sh
 
+# Setup java, installed by asdf
 . ~/.asdf/plugins/java/set-java-home.zsh
---
+++ oh-my-zsh/path.zsh
@@ -0,0 +1,2 @@
+export ZSH="/Users/dsrees/.oh-my-zsh"
+# export PATH="$ZSH/bin:$PATH"
