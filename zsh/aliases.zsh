fefcbba71711a0dc6a2b046d142770d55a03573a
dsrees <daniel.rees18@gmail.com>
2024-04-12 10:32:55 -0400

    Updates before new laptop

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,3 +6,6 @@ alias cls='clear' # Good 'ol Clear Screen command
 alias vup="vagrant up"
 alias vssh="vagrant ssh"
 alias vhalt="vagrant halt"
+alias gig="gigalixir"
+
+alias salt-local='sudo salt-call  --local --pillar-root=/Users/dsrees/dev/clustertruck/salt/pillar --file-root=/Users/dsrees/dev/clustertruck/salt/salt'
