5969c915fb82a1f9cbb390d9971cd70c372438e4
dsrees <daniel.rees18@gmail.com>
2024-11-21 10:18:18 -0500

    Updated z to use zsh-z instead

--- z/install.sh
+++ z/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-if ! [[ -d ~/dev/tools/z_jump_around ]]; then
-  # Install z. Update for latest
-  git clone git@github.com:rupa/z.git ~/dev/tools/z_jump_around
+if ! [[ -d ~/dev/tools/zsh-z ]]; then
+  # Install zsh-z. Update for latest
+  git clone git@github.com:agkozak/zsh-z.git ~/dev/tools/zsh-z
 fi
